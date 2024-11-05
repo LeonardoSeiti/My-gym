ecb53f228488eec1f6c28e466e666e9339257396
LeoSeiti <Leonardoseiti1@gmail.com>
2024-11-05 18:38:07 -0300

    first commit

---
+++ src/main/java/br/com/fiap/projdiamantegym/ProjDiamanteGymApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.projdiamantegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDiamanteGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDiamanteGymApplication.class, args);
+	}
+
+}
